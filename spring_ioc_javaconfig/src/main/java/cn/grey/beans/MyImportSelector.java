a31f89dd4bc7e3b932996fb4c2c034116fc9b7dc
gugibv <gugibv@163.com>
2023-03-31 00:15:06 +0800

    update

--- spring_ioc_javaconfig/src/main/java/cn/grey/beans/MyImportSelector.java
+++ spring_ioc_javaconfig/src/main/java/cn/grey/beans/MyImportSelector.java
@@ -9,6 +9,6 @@ public class MyImportSelector implements ImportSelector {
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
         // 可以以字符串的形式注册多个Bean
         // 字符串必须是类的完整限定名  getBean不能根据名字获取获取的， 必须要根据类型获取
-        return new String[]{"cn.tulingxueyuan.beans.Person",Wife.class.getName()};
+        return new String[]{"cn.grey.beans.Person",Wife.class.getName()};
     }
 }
